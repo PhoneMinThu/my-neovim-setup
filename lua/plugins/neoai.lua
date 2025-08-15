97cd6fc2b126cb50f607f9a9a73f8ec96d2841de
phoneminthu <mr.phoneminthu@gmail.com>
2025-08-16 03:43:32 +0630

    feat(onedarkpro): setup onedarkpro

--- lua/plugins/neoai.lua
+++ lua/plugins/neoai.lua
@@ -6,7 +6,10 @@ local openai_config = function()
       model = "o4-mini",
       api_key = OPENAI_API_KEY,
       max_completion_tokens = 4096 * 2,
-      addition_kwargs = {},
+      api_call_delay = 5000,
+      additional_kwargs = {
+        reasoning_effort = "medium",
+      },
     },
     chat = {
       database_path = vim.fn.stdpath("data") .. "/neoai.db",
@@ -15,22 +18,28 @@ local openai_config = function()
 end
 
 local groq_config = function()
-  local OPENAI_API_KEY = require("config.env").OPENAI_API_KEY
   local GROQ_API_KEY = require("config.env").GROQ_API_KEY
   require("neoai").setup({
     preset = "groq",
     api = {
-      model = "deepseek-r1-distill-llama-70b",
+      model = "openai/gpt-oss-20b",
       api_key = GROQ_API_KEY,
-      max_completion_tokens = 4096 * 2,
-      addition_kwargs = {},
+      max_completion_tokens = 8192,
+      api_call_delay = 5000,
+      additional_kwargs = {
+        reasoning_effort = "medium",
+      },
+    },
+    chat = {
+      database_path = vim.fn.stdpath("data") .. "/neoai.db",
     },
   })
 end
 
 return {
   {
-    dir = "/media/phonemt/linux_data/unix_dev/neovim-plugins/neoai.nvim/neoai.nvim",
+    -- dir = "/media/phonemt/linux_data/unix_dev/neovim-plugins/neoai.nvim/neoai.nvim",
+    "nvim-neoai/neoai.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
