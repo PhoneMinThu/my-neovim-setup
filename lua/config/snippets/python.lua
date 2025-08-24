1ea3e4b8b15d8538d0167d62f0de9fb110f159e8
phoneminthu <mr.phoneminthu@gmail.com>
2025-08-24 10:17:08 +0630

    refactor(space): spacing testing

--- lua/config/snippets/python.lua
+++ lua/config/snippets/python.lua
@@ -4,34 +4,34 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("python", {
-	s("drfser", {
-		t({ "from rest_framework import serializers", "", "" }),
-		t({ "class " }),
-		i(1, "MySerializer"),
-		t("(serializers.ModelSerializer):"),
-		t({ "", "    class Meta:" }),
-		t({ "", "        model = " }),
-		i(2, "MyModel"),
-		t({ "", "        fields = [" }),
-		i(3, "'field1', 'field2'"),
-		t("]"),
-	}),
+    s("drfser", {
+        t({ "from rest_framework import serializers", "", "" }),
+        t({ "class " }),
+        i(1, "MySerializer"),
+        t("(serializers.ModelSerializer):"),
+        t({ "", "    class Meta:" }),
+        t({ "", "        model = " }),
+        i(2, "MyModel"),
+        t({ "", "        fields = [" }),
+        i(3, "'field1', 'field2'"),
+        t("]"),
+    }),
 
-	s("drfview", {
-		t({ "from rest_framework import viewsets", "", "" }),
-		t({ "class " }),
-		i(1, "MyViewSet"),
-		t("(viewsets.ModelViewSet):"),
-		t({ "", "    queryset = " }),
-		i(2, "MyModel.objects.all()"),
-		t({ "", "    serializer_class = " }),
-		i(3, "MySerializer"),
-	}),
-	s("cls_init", {
-		t({ "class " }),
-		i(1, "MyClass"),
-		t({ ":" }),
-		t({ "", "   def __init__(self):" }),
-		t({ "", "        pass" }),
-	}),
+    s("drfview", {
+        t({ "from rest_framework import viewsets", "", "" }),
+        t({ "class " }),
+        i(1, "MyViewSet"),
+        t("(viewsets.ModelViewSet):"),
+        t({ "", "    queryset = " }),
+        i(2, "MyModel.objects.all()"),
+        t({ "", "    serializer_class = " }),
+        i(3, "MySerializer"),
+    }),
+    s("cls_init", {
+        t({ "class " }),
+        i(1, "MyClass"),
+        t({ ":" }),
+        t({ "", "   def __init__(self):" }),
+        t({ "", "        pass" }),
+    }),
 })
