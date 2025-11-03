b68fe5f4a488be36db3d506c674133546de5e286
phoneminthu <mr.phoneminthu@gmail.com>
2025-11-03 08:39:24 +0630

    On branch master Your branch is ahead of 'origin/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
          modified:   lua/plugins/colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,14 +7,20 @@ return {
     },
     {
         "rebelot/kanagawa.nvim",
-        config = function()
-            vim.cmd("colorscheme  kanagawa-wave")
-        end,
+        -- config = function()
+        --     vim.cmd("colorscheme  kanagawa-wave")
+        -- end,
     },
     {
         "olimorris/onedarkpro.nvim",
+        config = function()
+            vim.cmd("colorscheme onedark_dark")
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
         -- config = function()
-        --     vim.cmd("colorscheme onedark")
+        --     vim.cmd("colorscheme tokyonight-night")
         -- end
     },
 }
